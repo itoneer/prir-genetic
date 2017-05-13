2174fe7fe2abd9628ef41bd2e3081b00fdfef6ca
itoneer <skamin26@gmail.com>
2017-05-13 16:49:07 +0200

    initial
    Klasa główna + parę adnotacji

---
+++ PRiR-Genetic/src/prir/genetic/PRiRGenetic.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prir.genetic;
+
+/**
+ *
+ * @author itoneer
+ */
+public class PRiRGenetic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* do zrobienia:
+            - parsowanie argumentów
+            (wielkość populacji, ilość pokoleń, dokładność/ilość
+             pokoleń bez zmian do stwierdzenia stagnacji itp.)
+            - ustalenie: funkcji przystosowania, prawdop. krzyżowania i mutacji
+            - formy krżyżowania
+            - rozbicia na moduły
+        */
+    }
+    
+}
